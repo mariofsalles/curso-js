e9907fdb914e2a41bfc1c9aeb6818a82c4c84d6d
Mario Salles <mariofsalles@gmail.com>
2018-12-23 18:30:09 +0000

    Aula 03 - Variáveis

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -1,7 +1,11 @@
 // Aula 1
-alert("Mensagem enviada pelo Alert");
-console.log("Mensagem envida pelo console");
+// alert("Mensagem enviada pelo Alert");
+// console.log("Mensagem envida pelo console");
 
 // Aula 2
-// Aula 3
 
+// Aula 3
+var latitude = 40.87663;
+var longitude = -8.08373;
+console.log(latitude);
+console.log(longitude);
