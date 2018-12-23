28c8d68d54c8bc949149782a3609f2b613acba25
Mario Salles <mariofsalles@gmail.com>
2018-12-23 23:26:07 +0000

    Aula 08 - Lista de exercícios #1

---
+++ conteudo/js/ex1.js
@@ -0,0 +1,29 @@
+// 1 -
+console.log("O arquivo 'ex1.js'  está funcionando");
+
+console.log("----------------------------------")
+// 2 -
+var nome = 'Mário';
+var idade = '39';
+console.log("Olá, eu me chamo "+ nome +", tenho " + idade +" anos e estou estudando Javascript");
+
+console.log("----------------------------------")
+// 3 -
+var aluno = 'João';
+var matricula = '123456';
+var nota1 = 8.0;
+var nota2 = 9.0;
+var media = (nota1+nota2)/2;
+console.log("O aluno "+ aluno +", matrícula " + matricula + ", obteve a média final " + media);
+
+console.log("----------------------------------")
+// 4 -
+var telefone = '34443344';
+var tamanho = telefone.length==9;
+console.log('Resultado do teste: '+ tamanho);
+
+console.log("----------------------------------")
+// 5 -
+var base = 32;
+var expoente = 6;
+console.log("O resultado da potência cujo a base é " + base + " e a potência " + expoente + " é " + Math.pow(base, expoente));
