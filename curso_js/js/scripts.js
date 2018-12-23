79dbfa6da675132a87912598a40938617f144050
Mario Salles <mariofsalles@gmail.com>
2018-12-23 18:56:10 +0000

    Aula 04 - Strings

--- curso_js/js/scripts.js
+++ curso_js/js/scripts.js
@@ -5,7 +5,22 @@
 // Aula 2
 
 // Aula 3
-var latitude = 40.87663;
-var longitude = -8.08373;
-console.log(latitude);
-console.log(longitude);
+// var latitude = 40.87663;
+// var longitude = -8.08373;
+// console.log(latitude);
+// console.log(longitude);
+
+// Aula 4 - Strings
+var nome = "João"; // podem ser usadas aspas duplas
+var sobrenome = 'Gomes'; // ou aspas simples
+var cpf = '111.111.111-11';
+var telefone = "998887655";
+var ddd = '21';
+var email = 'joao@gmail.com';
+var biografia = "João começou a sua carreira como desenvolvedor web em 2010 e já participou de importantes projetos, como...";
+
+console.log('Nome Completo: '+nome+' '+sobrenome);
+console.log('Telefone: ' + '('+ddd +')'+telefone);
+console.log('Primeira letra do nome: ' + nome[0]);
+console.log('Última letra do nome: ' + nome[3]);
+console.log('Numero de caracteres do telefone: '+ telefone.length);
