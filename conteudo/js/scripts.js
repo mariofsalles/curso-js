4feeb1e90614a47d7b280dae104e48939a64e498
Mario Salles <mariofsalles@gmail.com>
2018-12-23 20:52:43 +0000

    Aula 06 - Booleanos

--- conteudo/js/scripts.js
+++ conteudo/js/scripts.js
@@ -25,48 +25,75 @@
 // console.log('Numero de caracteres do telefone: '+ telefone.length);
 
 // Aula 05
-var num1 = 20;
-var num2 = 3;
+// var num1 = 20;
+// var num2 = 3;
 
-var soma = num1 + num2;
-var subtracao = num1 - num2;
-var divisao = num1 / num2;
-var multiplicacao = num1 * num2;
+// var soma = num1 + num2;
+// var subtracao = num1 - num2;
+// var divisao = num1 / num2;
+// var multiplicacao = num1 * num2;
 
-var media = (num1 + num2) / 2;
-console.log('A média é: ' + media);
+// var media = (num1 + num2) / 2;
+// console.log('A média é: ' + media);
 
-console.log(Math.pow(2,4));
+// console.log(Math.pow(2,4));
 
-var arredonda = Math.round(2.7); // Arredondamento.
-var arredonda_cima = Math.ceil(4.3) // Arredondamento para cima.
-var arredonda_baixo = Math.floor(4.6) // Arredondamento para baixo.
-var raiz = Math.sqrt(36) // Raiz quadrada.
+// var arredonda = Math.round(2.7); // Arredondamento.
+// var arredonda_cima = Math.ceil(4.3) // Arredondamento para cima.
+// var arredonda_baixo = Math.floor(4.6) // Arredondamento para baixo.
+// var raiz = Math.sqrt(36) // Raiz quadrada.
 
-console.log('Arredoonda 2.7: ' + arredonda + 
-            '\nArredoonda cima 4.3: '+ arredonda_cima + 
-            '\nArredoonda baixo 4.6: '+ arredonda_baixo + 
-            '\nRaiz 36: '+raiz);
+// console.log('Arredoonda 2.7: ' + arredonda + 
+//             '\nArredoonda cima 4.3: '+ arredonda_cima + 
+//             '\nArredoonda baixo 4.6: '+ arredonda_baixo + 
+//             '\nRaiz 36: '+raiz);
   
-var increment = 20;
-increment += 5; 
-console.log(increment); 
+// var increment = 20;
+// increment += 5; 
+// console.log(increment); 
 
-increment = 10;
-increment++;
-console.log(increment);
+// increment = 10;
+// increment++;
+// console.log(increment);
 
-var ddd = 21;
-var telefone = 998887655;
-var ddd_string = ddd.toString();
-var tel_string = telefone.toString();
-var telefone_completo = '('+ddd_string +')' + tel_string;
-console.log('Telefone Completo: ' + telefone_completo);
+// var ddd = 21;
+// var telefone = 998887655;
+// var ddd_string = ddd.toString();
+// var tel_string = telefone.toString();
+// var telefone_completo = '('+ddd_string +')' + tel_string;
+// console.log('Telefone Completo: ' + telefone_completo);
 
-var idade = '17';
-idade = parseInt(idade);
-console.log('A idade é: ' + idade);
-idade++
-console.log('A idade incrementada: ' + idade);
+// var idade = '17';
+// idade = parseInt(idade);
+// console.log('A idade é: ' + idade);
+// idade++
+// console.log('A idade incrementada: ' + idade);
 
+// Aula 06
+/* 
+var idade = 65;
+var teste = idade < 60;
+console.log(teste);
 
+var idade = 45;
+var teste = idade == 45;
+console.log(teste);
+
+var idade = '45';
+var teste = idade === 45;
+console.log(teste);
+
+var idade = '45';
+var teste = idade !== 45;
+console.log(teste); 
+*/
+
+var v_type = true;
+var v_numero = 4.55;
+var idade = 21;
+var v_boolean = idade > 18;
+
+console.log(typeof v_type);
+console.log(typeof idade);
+console.log(typeof v_numero); 
+console.log(typeof v_boolean);
