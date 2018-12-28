48b4322ee1e6b234a3f5b03bb30611f2a4a14bea
Mario Salles <mariofsalles@gmail.com>
2018-12-28 16:16:07 +0000

    Aula 24 - BOM

--- conteudo/js/scripts.js
+++ conteudo/js/scripts.js
@@ -358,44 +358,51 @@ console.log(teste);
 
 // Aula 22
 
-var socio = false;
-var idade = 25;
-if (socio == true || idade >= 65){
-  console.log ('O ingresso é gratuito');
-} else {
-  if (idade < 18){
-  console.log('Preço a pagar R$6,00');
-  } else {
-  console.log('Preço a pagar R$12,00');
-  }
-}
-console.log('--------------------------');
-var funcionarios = [
-  {
-    'nome': 'Carlos Henrique da Silva',
-    'idade': 45,
-    'filhos': ['Mariana Alves da Silva', 'Fernanda Alves da Silva']
-  },
-  {
-    'nome': 'Maria Helena Pereira',
-    'idade': 32,
-    'filhos': undefined//['João Pedro Pereira de Souza']
-  },
+// var socio = false;
+// var idade = 25;
+// if (socio == true || idade >= 65){
+//   console.log ('O ingresso é gratuito');
+// } else {
+//   if (idade < 18){
+//   console.log('Preço a pagar R$6,00');
+//   } else {
+//   console.log('Preço a pagar R$12,00');
+//   }
+// }
+// console.log('--------------------------');
+// var funcionarios = [
+//   {
+//     'nome': 'Carlos Henrique da Silva',
+//     'idade': 45,
+//     'filhos': ['Mariana Alves da Silva', 'Fernanda Alves da Silva']
+//   },
+//   {
+//     'nome': 'Maria Helena Pereira',
+//     'idade': 32,
+//     'filhos': undefined//['João Pedro Pereira de Souza']
+//   },
 
-  {
-    'nome': 'José Feliciano Maia',
-    'idade': 39,
-    'filhos': ['Felipe Ferreira Maia', 'Fábio Ferreira Maia', 'João Ferreira Maia']
-  }
-];
+//   {
+//     'nome': 'José Feliciano Maia',
+//     'idade': 39,
+//     'filhos': ['Felipe Ferreira Maia', 'Fábio Ferreira Maia', 'João Ferreira Maia']
+//   }
+// ];
 
-var list_element = document.getElementById('filhos');
-list_element.innerHTML = "";
-for (var count0 = 0; count0< funcionarios.length; count0++) {
-  if (funcionarios[count0].filhos) {
-    var lista_filhos = funcionarios[count0].filhos;
-    for (var count1 = 0; count1 < lista_filhos.length; count1++) {
-      list_element.innerHTML += '<li>'+lista_filhos[count1]+ ' - filho de: '+funcionarios[count0].nome+'</li>';
-    }
-  }
-}
+// var list_element = document.getElementById('filhos');
+// list_element.innerHTML = "";
+// for (var count0 = 0; count0< funcionarios.length; count0++) {
+//   if (funcionarios[count0].filhos) {
+//     var lista_filhos = funcionarios[count0].filhos;
+//     for (var count1 = 0; count1 < lista_filhos.length; count1++) {
+//       list_element.innerHTML += '<li>'+lista_filhos[count1]+ ' - filho de: '+funcionarios[count0].nome+'</li>';
+//     }
+//   }
+// }
+
+//Aula 24
+window.onmousemove = function(e) {
+if (e.pageY < 5 ){
+  alert('Não perca os descontos exclusivos promocionais');
+};
+};
