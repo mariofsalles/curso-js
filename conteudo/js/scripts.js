9a0de889c6502a340cc78f161076dd8deab53063
Mario Salles <mariofsalles@gmail.com>
2019-01-01 20:13:53 +0000

    Aula 50 - AJAX - Request

--- conteudo/js/scripts.js
+++ conteudo/js/scripts.js
@@ -934,30 +934,42 @@ console.log(teste);
 // Aula 47
 // json: formato de armazanamento e transferencia 
 
-  var funcionario = {
-    'nome': "Fernanda Costa",
-    'd_nascimento': '1988-10-01',
-    'CPF': '111.111.111-11'
+  // var funcionario = {
+  //   'nome': "Fernanda Costa",
+  //   'd_nascimento': '1988-10-01',
+  //   'CPF': '111.111.111-11'
+  // };
+
+  // var funcionario_json = JSON.stringify(funcionario);
+  // console.log(funcionario_json); // -> é um string
+  // var funcionario_obj = JSON.parse(funcionario_json);
+  // console.log(funcionario_obj); 
+  // console.log('-----------------');
+  // var grupo_A = ['Real Madrid', 'Porto', 'Liverpool', 'Manchester United'];
+  // console.log(JSON.stringify(grupo_A));
+  // console.log(typeof JSON.stringify(grupo_A)); // O console mostrará "string"
+  // console.log('-----------------');
+
+
+  // var info_temp = '{ "coord": { "lon": 145.77, "lat": -16.92 }, "weather": [{ "id": 802, "main": "Clouds", "description": "scattered clouds", "icon": "03n" }], "base": "stations", "main": { "temp": 300.15, "pressure": 1007, "humidity": 74, "temp_min": 300.15, "temp_max": 300.15 }, "visibility": 10000, "wind": { "speed": 3.6, "deg": 160 }, "clouds": { "all": 40 }, "dt": 1485790200, "sys": { "type": 1, "id": 8166, "message": 0.2064, "country": "AU", "sunrise": 1485720272, "sunset": 1485766550 }, "id": 2172797, "name": "Cairns", "cod": 200 }';
+  // var info = JSON.parse(info_temp);
+  // console.log(info);
+  // console.log('-----------------');
+  // console.log('A umidade é de ' +info.main.humidity+'%');
+
+  // Aula 50
+
+  // para versões internet explorer anterior a 6 é necessário declarar a variavel numa estrutura considicional
+  var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP"); 
+
+  xhttp.onreadystatechange = function () { // ON_READY_STATE_CHANGE -> testa os estados e status
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+    }
   };
 
-  var funcionario_json = JSON.stringify(funcionario);
-  console.log(funcionario_json); // -> é um string
-  var funcionario_obj = JSON.parse(funcionario_json);
-  console.log(funcionario_obj); 
-  console.log('-----------------');
-  var grupo_A = ['Real Madrid', 'Porto', 'Liverpool', 'Manchester United'];
-  console.log(JSON.stringify(grupo_A));
-  console.log(typeof JSON.stringify(grupo_A)); // O console mostrará "string"
-  console.log('-----------------');
-
-
-  var info_temp = '{ "coord": { "lon": 145.77, "lat": -16.92 }, "weather": [{ "id": 802, "main": "Clouds", "description": "scattered clouds", "icon": "03n" }], "base": "stations", "main": { "temp": 300.15, "pressure": 1007, "humidity": 74, "temp_min": 300.15, "temp_max": 300.15 }, "visibility": 10000, "wind": { "speed": 3.6, "deg": 160 }, "clouds": { "all": 40 }, "dt": 1485790200, "sys": { "type": 1, "id": 8166, "message": 0.2064, "country": "AU", "sunrise": 1485720272, "sunset": 1485766550 }, "id": 2172797, "name": "Cairns", "cod": 200 }';
-  var info = JSON.parse(info_temp);
-  console.log(info);
-  console.log('-----------------');
-  console.log('A umidade é de ' +info.main.humidity+'%');
-
-
+  xhttp.open("GET", "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+  xhttp.send();
 
 
 });
